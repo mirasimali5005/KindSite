57bca6dd054a9c5cabe1efd95b9c36cbbc5c8318
alimir16 <mirasim.ali@mail.utoronto.ca>
2025-10-25 15:08:04 -0400

    Sync with main and ensure .env ignored

--- server/app.py
+++ server/app.py
@@ -78,6 +78,11 @@ def cu_expand():
 def healthz():
     return jsonify({"ok": True})
 
+
+@app.get("get-actions", methods=["GET"])
+def get_actions():
+    pass
+
 if __name__ == "__main__":
     host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", "5000"))
